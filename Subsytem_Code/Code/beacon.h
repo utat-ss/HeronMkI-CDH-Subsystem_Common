1166b1568fb335ae9ace3ad287d2536216aac168
bwuzhang <bwuzhang@gmail.com>
2016-01-30 15:54:42 -0500

    Added beacon, havn't tested.

---
+++ Subsytem_Code/Code/beacon.h
@@ -0,0 +1,9 @@
+/*
+ * beacon.h
+ *
+ * Created: 2016/1/30 14:42:40
+ *  Author: Chris Zhang
+ */ 
+#include <stdint.h>
+#include <stdbool.h>
+#include "trans_lib.h"
---
+++ Subsytem_Code/Code/morse.h
@@ -0,0 +1,9 @@
+/*
+ * morse.h
+ *
+ * Created: 2016/1/30 14:38:40
+ *  Author: Baiwu
+ */ 
+#include <iostream>
+#include <math.h>
+#include <fstream>
